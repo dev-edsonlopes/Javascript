74995a51617c5dc9d0e6c84b986a9e14430a3622
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-23 23:08:14 -0300

    Create aula01.js

---
+++ aula01.js
@@ -0,0 +1,9 @@
+console.log('Alô mundo') // Comando de saída.
+console.log('Fim!')
+
+//console.log('Esta linha está comentada')
+
+/*
+console.log('Comentário de várias linhas')
+console.log('Fim!')
+*/
