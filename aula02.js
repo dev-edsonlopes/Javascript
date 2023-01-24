a3215c5629be23858f177ce43af20cff2cb6a4cd
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-24 20:25:15 -0300

    Create aula02.js

---
+++ aula02.js
@@ -0,0 +1,7 @@
+"use strict" // Modo estrito.
+//nome = "Edson" //(Erro)
+
+let nome = "Edson" // No strict mode, é obrigado declarar o tipo da variável.
+console.log('Novo curso de Javascript Moderno') // Saída tipo string.
+console.log(nome) // Saída do valor da variável.
+console.log("Nome: "+nome) // Concatenar saída: string + variável.
