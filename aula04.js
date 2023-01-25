b3eadbb6cbc76658caacaf96bd3f7037ae5bbbb1
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-25 07:18:11 -0300

    Create aula04.js

---
+++ aula04.js
@@ -0,0 +1,36 @@
+//Operadores Matemáticos em Javascript
+
+/*
+    Adição                      +
+    Subtração                   -        
+    Multiplicação               *
+    Divisão                     /
+
+    resto ou modulo             %
+
+    Incremento                  ++
+    Decremento                  --
+
+    Atribuição                  =
+    Attr com adição             +=
+    Attr com Subtração          -=
+    Attre com Multiplicacão     *=
+    Attr com Divisão            /=
+*/
+
+let num1 = 10
+let num2 = 5
+let res = 0
+
+res = num1 + num2
+console.log(num1+' + '+num2, ' = ', res)
+
+res = num1 - num2
+console.log(num1+' - '+num2, ' = ', res)
+
+res = num1 * num2
+console.log(num1+' * '+ num2, ' = ', res)
+
+res = num1 / num2
+console.log(num1+' / '+num2, ' = ', res)
+
