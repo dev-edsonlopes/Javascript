2d83cc79da5bdf535e95885f93e9d77d6babfe93
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-28 11:57:20 -0300

    Update aula04.js

--- aula04.js
+++ aula04.js
@@ -5,7 +5,8 @@
     Subtração                   -        
     Multiplicação               *
     Divisão                     /
-
+    
+    exponeciação                **
     resto ou modulo             %
 
     Incremento                  ++
@@ -18,19 +19,47 @@
     Attr com Divisão            /=
 */
 
+// declando as variáveis.
 let num1 = 10
 let num2 = 5
 let res = 0
 
+console.log('-----------------------')
+console.log(num1, typeof(num1))
+console.log(num1, typeof (num1))
+console.log(res,'', typeof(res))
+console.log('-----------------------')
 res = num1 + num2
-console.log(num1+' + '+num2, ' = ', res)
+console.log(num1+' + '+num2+ ' = ', res)
 
 res = num1 - num2
-console.log(num1+' - '+num2, ' = ', res)
+console.log(num1+' - '+num2+ ' = ', res)
 
 res = num1 * num2
-console.log(num1+' * '+ num2, ' = ', res)
+console.log(num1+' * '+ num2+ ' = ', res)
+
 
 res = num1 / num2
-console.log(num1+' / '+num2, ' = ', res)
+console.log(num1+' / '+num2+ ' = ', res)
 
+console.log('-----------------------')
+
+
+console.log('Incremento', ++num1) // pre-incremento
+console.log('Decremento', --num1) // pre-decremento
+
+console.log('-----------------------')
+
+console.log('Attr com adição', num1 += num1)
+console.log('Attr com subtração', num1 -= num1)
+console.log('Attr com multiplicação', num2 *= num2)
+console.log('Attr com divisão', num2 /= num2)
+
+console.log('-----------------------')
+// Ordem de precedencia dos operadores;
+
+/*
+    1- negação / incremento / decremento;
+    1- multiplicação / divisão / resto ou módulo;
+    2- adição / subtração;
+*/ 
