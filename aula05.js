2f73d21008108f07fc7f6ef0e0ff6dc99d9207e1
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-30 20:42:58 -0300

    Update aula05.js

--- aula05.js
+++ aula05.js
@@ -1,2 +1,15 @@
 //Operadores Relacionais em Javascript
 
+/*
+    Operadores de comparação:
+
+        >   maior que
+        =>  maior 'ou' igual que
+        <   menor que
+        <=  menor 'ou' igual que
+        ==  igual á
+        !=  diferente de
+
+        !   negação
+*/
+
