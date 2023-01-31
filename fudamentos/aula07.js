17904d641545a6dffe160d3062c2d219a6976456
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-30 22:01:03 -0300

    Update aula07.js

--- fudamentos/aula07.js
+++ fudamentos/aula07.js
@@ -24,9 +24,55 @@
     false or false  = false
 
 -----------------------------------
-    Tabela verdade (NOT --> !)
-    not true    = false
-    not false   = true
-    
+Tabela verdade (NOT --> !)
+not true    = false
+not false   = true
 
+-----------------------------------
 */
+
+const v = true
+const f = false
+
+// teste logico
+
+console.log('----')
+console.log('NOT:')
+console.log()
+console.log(!v)
+console.log(!f)
+//AND
+console.log('----')
+console.log('AND:')
+console.log()
+console.log(v && v)
+console.log(v && f)
+console.log(f && f)
+
+console.log('----')
+//OR
+console.log('OR:')
+console.log()
+console.log(v || v)
+console.log(v || f)
+console.log(f || f)
+console.log('----')
+
+// Teste Lógico + teste relacionais
+
+let n1 = 10
+let n2 = 5
+let n3 = 20
+let n4 = 10
+
+console.log((n1 > n2) && (n3 > n4)) // true
+console.log((n1 > n3) && (n3 > n2)) // false
+console.log((n2 > n4) && (n3 > n1)) // false
+console.log((n4 > n3) && (n2 > n1)) // false
+console.log('----')
+
+
+console.log((n1 > n2) || (n3 > n4)) // true
+console.log((n1 > n3) || (n3 > n2)) // true
+console.log((n2 > n4) || (n3 > n1)) // true
+console.log((n4 > n3) || (n2 > n1)) // true
