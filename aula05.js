ce912d95b85f1cef9078c19fa53c28442f615264
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-30 21:27:25 -0300

    Update aula05.js

--- aula05.js
+++ aula05.js
@@ -4,12 +4,33 @@
     Operadores de comparação:
 
         >   maior que
-        =>  maior 'ou' igual que
+        >=  maior 'ou' igual que
         <   menor que
         <=  menor 'ou' igual que
         ==  igual á
         !=  diferente de
 
         !   negação
+
+        *Obs: retorna valor booleano => true ou false
 */
 
+let a = 10
+let b = 8
+let c = 8
+
+console.log(a > b) //true
+console.log(b < c) //false
+
+console.log(a >= c)// true
+console.log(b <= c)// true
+
+console.log(a == c)// false
+console.log(b == c)// true
+
+console.log(a != c)// true
+console.log(b != c)// false
+
+console.log(!(b == c))// false
+
+//
