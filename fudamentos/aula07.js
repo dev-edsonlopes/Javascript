8c08fd8a24a93f9f7b2cad60b6bc92375221193c
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-30 21:40:40 -0300

    move archive

---
+++ fudamentos/aula07.js
@@ -0,0 +1,32 @@
+// Operadores Lógicos em Javascript
+
+/*
+    &&      and
+    ||      or
+    !       not
+
+    *Obs: retorna valor booleano => true ou false
+*/
+
+/*
+-----------------------------------
+    Tabela verdade (AND --> &&)
+    true and true   = true
+    true and false  = false
+    false and true  = false
+    false and false = false
+
+-----------------------------------
+    Tabela verdade (OR --> ||)
+    true or true    = true
+    true or false   = true
+    false or true   = true
+    false or false  = false
+
+-----------------------------------
+    Tabela verdade (NOT --> !)
+    not true    = false
+    not false   = true
+    
+
+*/
