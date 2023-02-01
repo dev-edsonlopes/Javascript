762de66d8c184101420db85459889271ab9d65d4
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-31 22:06:14 -0300

    Create aula09.js

---
+++ fudamentos/aula09.js
@@ -0,0 +1,34 @@
+// Pré Incremento e Pré decremento
+
+let num = 10
+console.log(num)
+console.log(++num) // Realiza a operação antes do resultado.
+console.log(++num)
+
+console.log(--num)
+console.log(--num)
+console.log('-----')
+
+//Pós Incremento e Pós decremento
+console.log('-----')
+
+num = 1
+console.log(num++) // Mostra o resultado depois realiza a operação
+console.log(num++)
+
+console.log(num--)
+console.log(num--)
+
+console.log('-----')
+// operador de inversão de valor
+num = -1 
+let x = -num 
+
+console.log(x)
+
+
+// concatenação
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2)
+console.log(num1 +''+ num2) // CONCATENAÇÃO
