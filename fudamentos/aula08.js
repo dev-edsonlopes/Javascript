d98425456decdccaa3dfee55e14b4be5ba7fa348
Edson Lopes Jr <dev_edson@outlook.com>
2023-01-31 21:42:05 -0300

    Create aula08.js

---
+++ fudamentos/aula08.js
@@ -0,0 +1,25 @@
+//Operadores Bitwise em Javascript
+
+/*
+    Bitwise AND                 &
+    Bitwise OR                  |
+    Bitwise XOR                 ^
+    Bitwise left shift          <<
+    Bitwise right shift         >>
+*/
+
+// Teste Bit a Bit
+
+let n1 = 10     //
+let n2 = 11     // 
+
+console.log('----------------------')
+console.log('10 =', n1.toString(2))
+console.log('11 =', n2.toString(2))
+console.log('----------------------')
+
+console.log(n1 & n2)
+console.log(n1 | n2)
+console.log(n1 ^ n2)
+console.log(n1 << 1)
+console.log(n1 >> 1)
