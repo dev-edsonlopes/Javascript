9a3c3e4b5239489b6cb2d1059ef9a173a36309fc
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-04 11:32:18 -0300

    Create aula11.js

---
+++ fudamentos/aula11.js
@@ -0,0 +1,13 @@
+// Operador typeOf
+
+// Retorna o tipo de dados.
+
+let v1 = 10
+let v2 ="10" 
+let v3 = v1 === v2
+let v4 = {nome : 'Edson'}
+
+console.log('valor:', v1, '- tipo:', typeof(v1))
+console.log('valor:', v2, '- tipo:', typeof(v2))
+console.log('valor:', v3, '- tipo:', typeof(v3))
+console.log('valor:', v4, '- tipo:', typeof(v4))
