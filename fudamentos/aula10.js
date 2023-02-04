3b05135cf3b6756ed2fb71cee266384052b3de39
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-04 11:32:09 -0300

    Create aula10.js

---
+++ fudamentos/aula10.js
@@ -0,0 +1,17 @@
+// Operador Ternário
+// teste lógico ? se verdadeiro : se falso;
+let parOuImparL = 10; 
+let resL = (!(parOuImparL % 2) ? 'Par' :  'Impar')
+
+
+
+console.log(parOuImparL,'é',resL)
+console.log('------------------------------')
+
+
+// teste relacional ? se verdadeiro : se falso;
+let parOuImparR = 7
+let resR = parOuImparR % 2 == 0 ? 'Par' : 'Impar'
+
+console.log(parOuImparR,'é',resR )
+console.log('------------------------------')
