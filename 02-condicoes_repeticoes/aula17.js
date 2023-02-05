4136eb43a1c35192ed0a34344206153f0f0fc5e7
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-05 16:18:25 -0300

    Create aula17.js

---
+++ 02-condicoes_repeticoes/aula17.js
@@ -0,0 +1,23 @@
+// Loop While em Javascript
+
+let n = 0
+
+while(n < 10) {
+    console.log(n)
+    n++
+}
+
+//Fatorial
+
+let num = 5
+let backup = num
+let fat = 1
+
+while(num >= 1) {
+    fat *= num
+    num--
+}
+console.log()
+console.log('Fatorial: '+backup+'! = '+fat)
+
+
