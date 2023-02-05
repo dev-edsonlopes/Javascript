6729b5d2ab7709a41f0d4396fe13434864f8d904
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-05 08:22:34 -0300

    rename past

---
+++ 02-condicoes_repeticoes/aula13.js
@@ -0,0 +1,16 @@
+// Condicional IF e IF ELSE em Javascript
+
+let num = 40
+
+if(num > 10) {
+    console.log('O número é maior que 10')
+    if(num > 50) {
+        console.log('O número é maior que 50')
+    }
+} else if (num > 5 ) {
+    console.log('O número está entre 6 e 10')
+} else {
+    console.log('O número é menor ou igual á 5')
+}
+
+console.log('Fim do programa!')
