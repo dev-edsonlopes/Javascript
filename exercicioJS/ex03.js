95b876f657cc35faca9581efed50e007a9517385
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-11 20:11:15 -0300

    Create ex03.js

---
+++ exercicioJS/ex03.js
@@ -0,0 +1,9 @@
+//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+
+const potencia = function (base, exp) {
+    
+    return base ** exp
+
+}
+
+console.log(potencia(2, 2))
