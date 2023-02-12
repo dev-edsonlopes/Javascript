6db2286cd065782e1cfda8e789e662490c671460
Edson Lopes Jr <dev_edson@outlook.com>
2023-02-12 14:08:36 -0300

    create aula 38

---
+++ 04-DOM/aula38.js
@@ -0,0 +1,8 @@
+const caixa = document.querySelector(".caixa")
+const cursos = document.querySelectorAll(".curso")
+
+console.log(caixa.firstElementChild)
+console.log(caixa.children[3])
+console.log(caixa.lastElementChild)
+
+console.log(document.getRootNode())
