44467bbd0b97aaf046bc4cd1837f4d8008e5fbdc
EdsonLopesJr <dev_edson@outlook.com>
2023-03-02 11:22:04 -0300

    Update aula50.js

--- 04-DOM/aula50.js
+++ 04-DOM/aula50.js
@@ -5,14 +5,18 @@ const btnReduzir = document.querySelector('#btnReduzir')
 const resultado = document.querySelector('#resultado')
 
 const elementos_array = [1,2,3,4,5]
-const ant = []
-const atu = []
+const res = []
+const el = []
 
 arr.innerHTML = "["+elementos_array+"]"
 
 btnReduzir.addEventListener('click', ()=> {
-    resultado.innerHTML = elementos_array.reduce((anterior, atual, pos) => {
-        return anterior+atual
+    resultado.innerHTML = elementos_array.reduce((result, elemento) => {
+        res.push(result)
+        el.push(elemento)
+        return result + elemento 
     })
+    resultado.innerHTML += "</br> Elemento: "+el
+     resultado.innerHTML += "</br> Retorno da Operação: "+res
 
 })
